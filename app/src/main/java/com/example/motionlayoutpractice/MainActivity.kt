acd5189434992b0a500a574f83d49188b5a1be41
xuyong1211 <xuyong1211@live.com>
2021-07-08 00:47:15 +0800

    学习MotionLayout

--- app/src/main/java/com/example/motionlayoutpractice/MainActivity.kt
+++ app/src/main/java/com/example/motionlayoutpractice/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,22 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val motion = findViewById<MotionLayout>(R.id.motion)
-        val rv1 = findViewById<RecyclerView>(R.id.rv_1)
-        rv1.layoutManager = LinearLayoutManager(this,RecyclerView.VERTICAL,false)
-        val adapter = SimpleTextAdapter(arrayListOf("content","content","content","content",
-                "content","content","content","content","content","content","content","content","content","content","content","content","content",
-                "content","content","content","content","content","content","content","content","content","content","content","content","content",
-                "content","content","content","content","content","content","content","content","content","content"))
-        adapter.setLitener {
+//        val rv1 = findViewById<RecyclerView>(R.id.rv_1)
+//        rv1.layoutManager = LinearLayoutManager(this,RecyclerView.VERTICAL,false)
+//        val adapter = SimpleTextAdapter(arrayListOf("content","content","content","content",
+//                "content","content","content","content","content","content","content","content","content","content","content","content","content",
+//                "content","content","content","content","content","content","content","content","content","content","content","content","content",
+//                "content","content","content","content","content","content","content","content","content","content"))
+//        adapter.setLitener {
+//            motion.transitionToState(R.id.mid_state)
+//        }
+//        rv1.adapter = adapter
+        findViewById<Button>(R.id.button).setOnClickListener {
             motion.transitionToState(R.id.mid_state)
         }
-        rv1.adapter = adapter
-
-        val rv2 = findViewById<RecyclerView>(R.id.rv_2)
+        val rv2 = findViewById<MyRecycletView>(R.id.rv_2)
         rv2.layoutManager = LinearLayoutManager(this,RecyclerView.VERTICAL,false)
-        rv2.adapter = SimpleTextAdapter(arrayListOf("content_2","content_2","content_2","content_2","content_2","content_2"
-                ,"content_2","content_2","content_2","content_2","content_2","content_2","content_2"))
-
+        rv2.adapter = SimpleTextAdapter(arrayListOf("content_2","content_2","content_2"))
 
     }
 }
---
+++ app/src/main/java/com/example/motionlayoutpractice/MyRecycletView.kt
@@ -0,0 +1,17 @@
+package com.example.motionlayoutpractice
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ *@Author： XY
+ *Date： 2021/7/5 23:00
+ *E-mail： xuyong1211@live.com
+ *Use: 用途
+ */
+class MyRecycletView(context: Context,attributeSet: AttributeSet)  :RecyclerView(context,attributeSet){
+
+}
